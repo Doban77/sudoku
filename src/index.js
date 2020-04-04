c6c61773f4c59bda410204e2bc49f5276ef964a1
Andrzej Dobkowski <bfgryfin@gmail.com>
2020-04-04 17:12:14 +0200

    feat: added iteration through boxes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 module.exports = function solveSudoku(matrix) {
-  for (let col = 0; column < matrix.length; col++) {
-    for (let row = 0; row < matrix.length; row++) {
-      if (matrix[col][row] === 0) {
-
+  let notPossible = {}, impossibleDigits;
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix.length; col++) {
+      if (matrix[row][col] === 0) {
+        notPossible = {};
+        for (let i=0; i < 9; i++) {
+          if (matrix[row][i] > 0) {
+            notPossible[matrix[row][i]] = true;
+          }
+          if (matrix[i][col] > 0) {
+            notPossible[matrix[i][col]] = true;
+          }
+        }
+        for (let colBox = col % 3; colBox < col % 3 +3; colBox++) {
+          for ( let rowBox = row % 3; rowBox < row % 3 + 3; rowBox++) {
+            if (matrix[colBox][rowBox]) {
+              notPossible[[colBox][rowBox]] = true;
+            }
+          }
+        }
       }
     }
   }
