2a673c3bd3edbcca8b3b87e095b9c5afe50a22ae
Andrzej Dobkowski <bfgryfin@gmail.com>
2020-04-04 13:19:49 +0200

    eat: added iteration through all items in sudoku grid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function solveSudoku(matrix) {
-  // start sudoku task
+  for (let col = 0; column < matrix.length; col++) {
+    for (let row = 0; row < matrix.length; row++) {
+      if (matrix[col][row] === 0) {
+
+      }
+    }
+  }
 }
